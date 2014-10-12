bbc604b82cba55b8348a9c1944c79b685d7115be
chimaine <tobias@rummelt-software.de>
2014-10-12 17:07:44 +0200

    Fix issues with negative zeros

--- src/main/java/net/mobtalker/mobtalkerscript/v2/value/MtsNumber.java
+++ src/main/java/net/mobtalker/mobtalkerscript/v2/value/MtsNumber.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013-2014 Chimaine
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -43,7 +43,8 @@ public final class MtsNumber extends MtsValue
         if ( Double.isNaN( value ) )
             return NaN;
         
-        return new MtsNumber( value );
+        int id = (int) value;
+        return value == id ? new MtsNumber( id ) : new MtsNumber( value );
     }
     
     public static MtsNumber of( int value )

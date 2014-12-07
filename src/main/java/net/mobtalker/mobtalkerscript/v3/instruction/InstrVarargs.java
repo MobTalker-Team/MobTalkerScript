77efc0c325b816b30aa28b41f3d6a0ff43899061
chimaine <tobias@rummelt-software.de>
2014-12-07 21:55:08 +0100

    Fix varargs stackSizeChange

--- src/main/java/net/mobtalker/mobtalkerscript/v3/instruction/InstrVarargs.java
+++ src/main/java/net/mobtalker/mobtalkerscript/v3/instruction/InstrVarargs.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013-2014 Chimaine
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,7 +28,6 @@ public class InstrVarargs extends MtsInstruction
     
     /* package */InstrVarargs( int count )
     {
-        assert count > 0;
         _count = count;
     }
     
@@ -46,7 +45,7 @@ public class InstrVarargs extends MtsInstruction
     @Override
     public int stackSizeChange()
     {
-        return _count;
+        return Math.max( _count, 1 );
     }
     
     // ========================================
@@ -54,7 +53,7 @@ public class InstrVarargs extends MtsInstruction
     @Override
     public String toString()
     {
-        return "VARARGS " + _count;
+        return Instructions.VARARG_NAME + " " + _count;
     }
     
     @Override

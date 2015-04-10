b4dfa511db08562bb6c6ec3915367f4d853c3650
chimaine <tobias@rummelt-software.de>
2015-04-10 19:53:33 +0200

    LoadM should also respect metatags

--- src/main/java/net/mobtalker/mobtalkerscript/v3/instruction/InstrLoadM.java
+++ src/main/java/net/mobtalker/mobtalkerscript/v3/instruction/InstrLoadM.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2013-2015 Chimaine
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -35,7 +35,7 @@ public final class InstrLoadM extends MtsIndexedInstruction
     {
         MtsValue table = frame.pop();
         MtsValue key = frame.getConstant( _index );
-        MtsValue method = table.get( key );
+        MtsValue method = table.get( key, true );
         
         frame.push( method );
         frame.push( table );
